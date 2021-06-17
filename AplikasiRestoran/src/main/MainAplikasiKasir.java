9d6ab86337c5723d25af349e8d75be625e506f10
penerbangkuda <agungmochm82@gmail.com>
2021-06-17 09:29:13 +0700

    Add Fitur Menambah dan Menampilkan Daftar Menu

--- AplikasiRestoran/src/main/MainAplikasiKasir.java
+++ AplikasiRestoran/src/main/MainAplikasiKasir.java
@@ -13,5 +13,26 @@ public class MainAplikasiKasir {
     
     public static void main(String [] args) { }
     
-    public void generateDaftarMenu() { }
+    public void generateDaftarMenu() {
+     daftarMenu = new DaftarMenu();
+        daftarMenu.tambahMenu(new Ramen("Ramen Seafood", 25000));
+        daftarMenu.tambahMenu(new Ramen("Ramen Original", 18000));
+        daftarMenu.tambahMenu(new Ramen("Ramen Vegetarian", 22000));
+        daftarMenu.tambahMenu(new Ramen("Ramen Karnivor", 28000));
+        daftarMenu.tambahMenu(new Kuah("Kuah Original "));
+        daftarMenu.tambahMenu(new Kuah("Kuah Internasional"));
+        daftarMenu.tambahMenu(new Kuah("Kuah Spicy Lada"));
+        daftarMenu.tambahMenu(new Kuah("Kuah Soto Padang"));
+        daftarMenu.tambahMenu(new Toping("Crab Stick Bakar", 6000));
+        daftarMenu.tambahMenu(new Toping("Chicken Katsu", 8000));
+        daftarMenu.tambahMenu(new Toping("Gyoza Goreng", 4000));
+        daftarMenu.tambahMenu(new Toping("Bakso Goreng", 7000));
+        daftarMenu.tambahMenu(new Toping("Enoki Goreng", 5000));
+        daftarMenu.tambahMenu(new Minuman("Jus Alpukat SPC", 10000));
+        daftarMenu.tambahMenu(new Minuman("Jus Stroberi", 11000));
+        daftarMenu.tambahMenu(new Minuman("Capucino Coffee", 15000));
+        daftarMenu.tambahMenu(new Minuman("Vietnam Dripp", 14000));
+
+        daftarMenu.tampilDaftarMenu();
+    }
 }
